53ff99a4465696378f2a9992f6c7b6beaa5c69e6
yuanhe0609 <1585397647@qq.com>
2024-09-26 14:00:59 +0800

    uodate redis

--- src/main/java/com/company/project/controller/UserController.java
+++ src/main/java/com/company/project/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.company.project.controller;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.company.project.entity.User;
 import com.company.project.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +21,13 @@ public class UserController {
 
     @Autowired
     UserMapper userMapper;
+    @Autowired
+    RedisTemplate redisTemplate;
 
     @RequestMapping("/login")
     public String login(){
-
+        redisTemplate.opsForValue().set("login", "1");
+        System.out.println(redisTemplate.opsForValue().get("login"));
         User user = new User();
         user.setName("test01");
         userMapper.addUser(user);
