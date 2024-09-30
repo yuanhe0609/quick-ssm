fe1e4f7ddc50ae7b361fa97c91f114d079f0c3b2
yuanhe0609 <1585397647@qq.com>
2024-09-30 09:28:09 +0800

    uodate swagger,logf4j

--- src/main/java/com/company/project/controller/UserController.java
+++ src/main/java/com/company/project/controller/UserController.java
@@ -5,12 +5,17 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.company.project.entity.User;
 import com.company.project.mapper.UserMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @description:
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @time: 2024/9/24
  */
 @RestController
+@Api(value = "userController")
 public class UserController {
 
     @Autowired
@@ -26,13 +32,13 @@ public class UserController {
     @Autowired
     RedisTemplate redisTemplate;
 
-    @Autowired
-    MongoTemplate mongoTemplate;
-
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     @RequestMapping("/login")
+    @ApiOperation(value = "根据id查询学生信息", notes = "查询学生", response = java.lang.String.class)
     public String login(){
         redisTemplate.opsForValue().set("login", "1");
-        System.out.println(redisTemplate.opsForValue().get("login"));
+        Object msg = redisTemplate.opsForValue().get("login");
+        log.info("slf4j print info msg:{}",msg);
         User user = new User();
         user.setName("test01");
         userMapper.addUser(user);
